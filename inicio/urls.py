0a36177e630f2f76794d5bc982d37809c100b4b7
Santiago <santi.leve10@gmail.com>
2023-11-18 19:09:43 -0500

    cambio

--- inicio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('crear/clase1/', views.crear_clase1, name='crear_clase1'),
-    # Define rutas para crear clases 2 y 3 y otras vistas
-    path('buscar/', views.buscar, name='buscar'),
-    path('exito/', views.exito, name='exito'),
-]
